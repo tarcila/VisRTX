a7c03f65aefa6f0c2d97fd7518f91d2f0c2560ef
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2025-09-25 17:54:00 -0400

    TSD: Cleanup HDRI loading
    For now, keep y-flipping images, but make sure to restore the do not flip stbi state
    Align the code to assume that we load RGB data only.

--- tsd/src/tsd/io/importers/detail/HDRImage.cpp
+++ tsd/src/tsd/io/importers/detail/HDRImage.cpp
@@ -35,22 +35,28 @@ bool HDRImage::import(std::string fileName)
     return false;
 
   if (extension == ".hdr") {
-    stbi_set_flip_vertically_on_load(1);
     int w, h, n;
-    float *imgData = stbi_loadf(fileName.c_str(), &w, &h, &n, STBI_rgb);
+    stbi_set_flip_vertically_on_load(1);
+    const float *imgData = stbi_loadf(fileName.c_str(), &w, &h, &n, STBI_rgb);
+    stbi_set_flip_vertically_on_load(0); // Restore default top-down orientation
     width = w;
     height = h;
-    numComponents = n;
-    pixel.resize(w * h * n);
-    std::memcpy(pixel.data(), imgData, w * h * n * sizeof(float));
-    stbi_image_free(imgData);
-    return width > 0 && height > 0
-        && (numComponents == 3 || numComponents == 4);
+    numComponents = 3; // because of STBI_rgb
+    if (width <= 0 || height <= 0 || n < 3) {
+      stbi_image_free(const_cast<float*>(imgData));
+      logError("import_HDRI] error importing HDR image: %s", fileName.c_str());
+      return false;
+    }
+
+    pixel.resize(w * h * 3);
+    std::memcpy(pixel.data(), imgData, w * h * 3 * sizeof(float));
+    stbi_image_free(const_cast<float*>(imgData));
+    return true;
 #ifdef _WIN32
   }
 #else
   } else {
-    int w, h, n;
+    int w, h;
     float *imgData;
     const char *err;
     int ret = LoadEXR(&imgData, &w, &h, fileName.c_str(), &err);
@@ -58,21 +64,26 @@ bool HDRImage::import(std::string fileName)
       logError("import_HDRI] error importing EXR: %s", err);
       return false;
     }
-    n = 4;
 
     width = w;
     height = h;
-    numComponents = n;
-    pixel.resize(w * h * n);
-    // flip-y
-    const size_t rowStride = w * n;
-    for (int y = 0; y < h; ++y) {
-      std::memcpy(pixel.data() + y * rowStride,
-          imgData + (h - y - 1) * rowStride,
-          rowStride * sizeof(float));
+    numComponents = 3;
+
+    pixel.resize(w * h * 3);
+    // LoadEXR returns a RGBA image, we want RGB.
+    // Vertical flip the image.
+    for (auto j = 0; j < h; ++j) {
+      for (auto i = 0; i < w; ++i) {
+        auto srcidx = 4 * (j * w + i);
+        auto dstidx = 3 * ((h - j - 1) * w + i);
+        pixel[dstidx + 0] = imgData[srcidx + 0];
+        pixel[dstidx + 1] = imgData[srcidx + 1];
+        pixel[dstidx + 2] = imgData[srcidx + 2];
+      }
     }
-    return width > 0 && height > 0
-        && (numComponents == 3 || numComponents == 4);
+    free(imgData);
+
+    return true;
   }
 #endif
 
