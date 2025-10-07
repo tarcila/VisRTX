3f4d5d1b6b334aa7143ce560ae563127e150694c
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2025-10-07 07:59:16 -0400

    Add support for transformation

--- tsd/src/tsd/io/serialization/export_SceneToUSD.cpp
+++ tsd/src/tsd/io/serialization/export_SceneToUSD.cpp
@@ -1,17 +1,25 @@
 // Copyright 2025 NVIDIA Corporation
 // SPDX-License-Identifier: Apache-2.0
-
 #ifndef TSD_USE_USD
 #define TSD_USE_USD 1
 #endif
 
-// tsd_io
-#include "tsd/io/serialization.hpp"
-// tsd_core
+// tsd
 #include "tsd/core/Logging.hpp"
+#include "tsd/io/serialization.hpp"
+// anari
+#include <anari/anari_cpp/ext/linalg.h>
+#include <anari/frontend/anari_enums.h>
+#include <anari/frontend/type_utility.h>
+
 #include "tsd/core/scene/Scene.hpp"
+#include "tsd/core/scene/objects/Array.hpp"
+#include "tsd/core/scene/objects/Material.hpp"
+#include "tsd/core/scene/objects/Sampler.hpp"
 #include "tsd/core/scene/objects/Surface.hpp"
-#if TSD_USE_USD
+
+// anari
+#include <anari/frontend/type_utility.h>
 // usd
 #include <pxr/base/vt/array.h>
 #include <pxr/usd/sdf/path.h>
@@ -20,11 +28,16 @@
 #include <pxr/usd/usd/stage.h>
 #include <pxr/usd/usdGeom/mesh.h>
 #include <pxr/usd/usdGeom/scope.h>
-#endif
+#include <pxr/usd/usdGeom/tokens.h>
+#include <pxr/usd/usdGeom/xformOp.h>
 // std
 #include <algorithm>
 #include <cstdio>
-#include <numeric>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace tsd::io {
 
@@ -40,28 +53,45 @@ static const auto allLayersPath = pxr::SdfPath("/AllLayers");
 
 static std::unordered_map<const Object *, pxr::SdfPath> objectToUsdPathMap;
 
-static pxr::SdfPath tsdSurfaceToUSD(
-    pxr::UsdStageRefPtr &stage, const Object *object)
+pxr::SdfPath allocateUniquePath(
+    pxr::UsdStagePtr stage, const pxr::SdfPath &path)
 {
-  if (auto it = objectToUsdPathMap.find(object);
+  const auto basePath = path.GetParentPath();
+  auto baseName = path.GetName();
+
+  int uniqueIndex = 0;
+  auto newPath = pxr::SdfPath::EmptyPath();
+  do {
+    auto uniqueName = baseName + "_" + std::to_string(uniqueIndex++);
+    newPath = basePath.AppendChild(pxr::TfToken(uniqueName.c_str()));
+  } while (stage->GetObjectAtPath(newPath));
+
+  return newPath;
+}
+
+static pxr::SdfPath tsdSurfaceToUSD(
+    pxr::UsdStageRefPtr &stage, const Surface *surface)
+{
+  if (auto it = objectToUsdPathMap.find(surface);
       it != objectToUsdPathMap.end()) {
     return it->second;
   }
 
   auto *anariGeom =
-      object->parameterValueAsObject<Geometry>(tokens::surface::geometry);
+      surface->parameterValueAsObject<Geometry>(tokens::surface::geometry);
   auto *anariMaterial =
-      object->parameterValueAsObject<Material>(tokens::surface::material);
+      surface->parameterValueAsObject<Material>(tokens::surface::material);
 
-  pxr::SdfPath objectPath =
-      allSurfacesPath.AppendChild(pxr::TfToken(object->name().c_str()));
+  pxr::SdfPath surfacePath =
+      allSurfacesPath.AppendChild(pxr::TfToken(surface->name().c_str()));
+  surfacePath = allocateUniquePath(stage, surfacePath);
 
   if (!anariGeom) { // && !anariMaterial) {
     return pxr::SdfPath::EmptyPath();
   }
 
   if (anariGeom->subtype() == tokens::geometry::triangle) {
-    auto usdGeomMesh = pxr::UsdGeomMesh::Define(stage, objectPath);
+    auto usdGeomMesh = pxr::UsdGeomMesh::Define(stage, surfacePath);
 
     auto triangles =
         anariGeom->parameterValueAsObject<Array>("vertex.position");
@@ -93,14 +123,9 @@ static pxr::SdfPath tsdSurfaceToUSD(
     usdGeomMesh.CreateFaceVertexIndicesAttr().Set(
         pxr::VtArray<int>(cbegin(faceVertexIndices), cend(faceVertexIndices)));
 
-  } else {
-    objectPath = pxr::SdfPath::EmptyPath();
-  }
+  objectToUsdPathMap.insert({surface, surfacePath});
 
-  if (!objectPath.IsEmpty()) {
-    objectToUsdPathMap.insert({object, objectPath});
-  }
-  return objectPath;
+  return surfacePath;
 }
 
 void export_SceneToUSD(Scene &scene, const char *filename)
@@ -116,53 +141,90 @@ void export_SceneToUSD(Scene &scene, const char *filename)
   }
 
   pxr::SdfPath currentPath = allLayersPath;
-
   std::unordered_set<pxr::SdfPath, pxr::SdfPath::Hash> existingNames;
 
   for (auto l : scene.layers()) {
     if (auto layer = l.second.ptr) {
       int currentLevel = -1;
+      std::stack<math::mat4> transformStack;
 
       layer->traverse(layer->root(), [&](const LayerNode &node, int level) {
+        // Handle depth traversal
         if (level == 0) {
           // special case for root -- output UsdGeomScope
           currentPath = currentPath.AppendChild(pxr::TfToken(l.first.c_str()));
           pxr::UsdGeomScope::Define(stage, currentPath);
-        } else {
-          if (node->isEmpty())
-            return true;
-          if (node->isObject()) {
-            auto object = node->getObject();
-            auto name = node->name();
-            if (name.empty())
-              name = object->name();
-            if (name.empty()) {
-              fprintf(stderr, "Don't know what to do yet");
-              std::exit(1);
-            }
+          transformStack.push(math::mat4(math::identity));
 
-            pxr::SdfPath objectPath =
-                currentPath.AppendChild(pxr::TfToken(name.c_str()));
+          return true;
+        }
 
-            if (auto insertPair = existingNames.insert(objectPath);
-                insertPair.second) {
-              switch (object->type()) {
-              case ANARI_SURFACE: {
-                if (auto meshPath = tsdSurfaceToUSD(stage, object);
-                    meshPath != pxr::SdfPath::EmptyPath()) {
-                  auto mesh = pxr::UsdGeomMesh::Define(stage, objectPath);
-                  mesh.GetPrim().GetReferences().AddInternalReference(meshPath);
-                }
-                break;
-              }
-              default:
-                break;
-              }
-            } else {
-              fprintf(stderr, "Collision with %s\n", objectPath.GetText());
+        if (level > currentLevel) {
+          for (int i = currentLevel; i < level; i++)
+            transformStack.push(transformStack.top());
+
+        } else if (level < currentLevel) {
+          for (int i = level; i < currentLevel; i++)
+            transformStack.pop();
+        }
+
+        currentLevel = level;
+
+        // Handle this node
+        if (node->isEmpty())
+          return true;
+        if (node->isTransform()) {
+          auto transform = node->getTransform();
+          transformStack.pop();
+          transformStack.push(math::mul(transformStack.top(), transform));
+        }
+        if (node->isObject()) {
+          auto object = node->getObject();
+          auto name = node->name();
+          if (name.empty())
+            name = object->name();
+          if (name.empty()) {
+            fprintf(stderr, "Don't know what to do yet");
+            std::exit(1);
+          }
+
+          auto objectPath = currentPath.AppendChild(pxr::TfToken(name.c_str()));
+          objectPath = allocateUniquePath(
+              stage, currentPath.AppendChild(pxr::TfToken(name.c_str())));
+
+          switch (object->type()) {
+          case ANARI_SURFACE: {
+            if (auto meshPath = tsdSurfaceToUSD(
+                    stage, static_cast<const Surface *>(object));
+                meshPath != pxr::SdfPath::EmptyPath()) {
+              auto mesh = pxr::UsdGeomMesh::Define(stage, objectPath);
+              mesh.GetPrim().GetReferences().AddInternalReference(meshPath);
+              auto xfm = transformStack.top();
+              mesh.AddXformOp(pxr::UsdGeomXformOp::TypeTransform)
+                  .Set(pxr::GfMatrix4d(xfm[0][0],
+                      xfm[0][1],
+                      xfm[0][2],
+                      xfm[0][3],
+                      xfm[1][0],
+                      xfm[1][1],
+                      xfm[1][2],
+                      xfm[1][3],
+                      xfm[2][0],
+                      xfm[2][1],
+                      xfm[2][2],
+                      xfm[2][3],
+                      xfm[3][0],
+                      xfm[3][1],
+                      xfm[3][2],
+                      xfm[3][3]));
             }
+            break;
+          }
+          default:
+            break;
           }
         }
+
         return true;
       });
     }
